cdd3ea0ba0643635a4fa1556a354a1249a8fbcce
ReiVillo52 <oliwierpietrzak123@gmail.com>
2025-04-21 09:29:45 +0200

    Add files via upload

---
+++ kraje/java.js
@@ -0,0 +1,10 @@
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const toggle = document.getElementById('menu-toggle');
+        const nav = document.querySelector('#main-nav ul');
+        
+        toggle.addEventListener('click', function () {
+            nav.classList.toggle('active');
+        });
+    });
+</script>
